5111c3e7c293c04444e8ce26f56e2f7750db9fbb
Viet Hoang <dvhoang2106@gmail.com>
2021-11-16 14:50:23 +0700

    deletae

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    introTitle: 'Hee luu cậu! Tớ là Diệu Phương xinh đẹp',
+    introTitle: 'Hee luu cậu! Tớ là Việt Hoàng',
     introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'okeeei',
+    btnIntro: 'okeeeiiii',
     title: 'Cậu yêu tớ có phải không 😙',
     desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là ny tớ đó nha :> ',
     btnYes: 'yêu ơi là iuuu <33',
@@ -12,5 +12,5 @@ const CONFIG = {
     mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
     messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/dieu.phuong.1808' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/dvhoang.2106/' 
 }
